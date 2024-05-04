7cdd6dfb6782f6c924d7857cd16c5c23179ded69
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-05-04 16:44:04 +0200

    1.5 update

---
+++ 1.5/Source/AchievementsExpanded/AchievementsExpanded/Tracker/Misc/ItemTracker.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using HarmonyLib;
+using Verse;
+using RimWorld;
+
+namespace AchievementsExpanded
+{
+	public class ItemTracker : TrackerBase
+	{
+		public ThingDef def;
+		public int count = 1;
+        Dictionary<ThingDef, int> thingList = new Dictionary<ThingDef, int>();
+		public bool mustHaveAll = false;
+
+        [Unsaved]
+		protected int triggeredCount; //Only for display
+
+		public override string Key => "ItemTracker";
+
+		public override Func<bool> AttachToLongTick => () => { return Trigger(); };
+		protected override string[] DebugText => new string[] { $"Def: {def?.defName ?? "None"}", $"Count: {count}" };
+
+		public ItemTracker()
+		{
+		}
+
+		public ItemTracker(ItemTracker reference) : base(reference)
+		{
+			def = reference.def;
+			count = reference.count;
+            thingList = reference.thingList;
+			mustHaveAll = reference.mustHaveAll;
+        }
+
+		public override bool UnlockOnStartup => Trigger();
+
+		public override void ExposeData()
+		{
+			base.ExposeData();
+			Scribe_Defs.Look(ref def, "def");
+			Scribe_Values.Look(ref count, "count", 1);
+            Scribe_Values.Look(ref mustHaveAll, "mustHaveAll", false);
+            Scribe_Collections.Look(ref thingList, "thingList", LookMode.Def, LookMode.Value);
+        }
+
+		public override (float percent, string text) PercentComplete => count > 1 ? ((float)triggeredCount / count, $"{triggeredCount} / {count}") : base.PercentComplete;
+
+		public override bool Trigger()
+		{
+			base.Trigger();
+			if (thingList.Count>0) {
+                bool playerHasIt = false;
+                foreach (KeyValuePair<ThingDef, int> set in thingList)
+                {
+                    playerHasIt = UtilityMethods.PlayerHas(set.Key, out int total, count);
+					if (mustHaveAll)
+					{
+                        if (!playerHasIt) { break; }
+                    }
+					else { if (playerHasIt) { break; } }
+                    
+                }
+                return playerHasIt;
+
+            } else { 
+				return UtilityMethods.PlayerHas(def, out triggeredCount, count);
+			}
+
+			
+		}
+	}
+}
